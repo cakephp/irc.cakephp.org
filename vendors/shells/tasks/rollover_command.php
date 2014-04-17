3315878706678b59d6199a2aa91562f28a99d8c1
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-04-16 20:54:43 -0400

    Create rollover_command.php

---
+++ vendors/shells/tasks/rollover_command.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Sample CommandTask for CakeBot.v1.0 This is the ~rollover command
+ *
+ *
+ * @package       cakebot
+ * @subpackage    cakebot.vendors.shells.tasks
+ */
+class RolloverCommandTask extends Object {
+
+	function execute($userName) {
+		return "/me plays dead";
+	}
+
+}
